b7dee15b3c0fef0d7d8b1b82af8f53a64e51835e
raphspies <raphspies@gmail.com>
2021-07-12 15:53:43 -0700

    added confirm_password custom validation

--- server/routes/newuser.js
+++ server/routes/newuser.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const newuser = express.Router();
-// const API = require('../controllers');
 const { check, validationResult } = require('express-validator');
-// const { check, body, validationResult, oneOf } = require('express-validator');
 
 newuser.post('/',
   [
-    check('name')
+    check('firstname')
+      .not()
+      .isEmpty()
+      .withMessage('Name is required'),
+    check('lastname')
       .not()
       .isEmpty()
       .withMessage('Name is required'),
@@ -20,13 +22,13 @@ newuser.post('/',
       .isLength({ min: 8, max: 20 })
       .withMessage('Password must be 8-20 characters long'),
     check('confirm_password')
-      // .matches('password')
-    // if (val !== "aA#123456") { //req.body.confirm_password) {
-    //       throw new Error("Passwords don't match");
-    //     } else {
-    //       return val;
-    //     }
-    //   }),
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords don't match");
+        } else {
+          return true;
+        }
+      }),
   ], (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
