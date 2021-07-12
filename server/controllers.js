0bbfc203cc7b56d93ff9d3ad185fe3b874a5f7ff
Andrew Munoz <ajmunoz411@gmail.com>
2021-07-12 15:58:28 -0700

    addAnnouncement and getUser added

--- server/controllers.js
+++ server/controllers.js
@@ -17,8 +17,20 @@ const controllers = {
 
     const queryStr = `
       WITH X AS (
-        INSERT INTO nexdoor.address (street_address, city, state, zipcode, neighborhood)
-        VALUES ('${streetAddress}', '${city}', '${state}', ${zipcode}, '${neighborhood}')
+        INSERT INTO nexdoor.address (
+          street_address,
+          city,
+          state,
+          zipcode,
+          neighborhood
+        )
+        VALUES (
+          '${streetAddress}',
+          '${city}',
+          '${state}',
+          ${zipcode},
+          '${neighborhood}'
+        )
         RETURNING address_id
       )
       INSERT INTO nexdoor.users (
@@ -32,7 +44,16 @@ const controllers = {
         avg_rating,
         profile_picture_url
       )
-      SELECT '${firstName}', '${lastName}', '${password}', '${email}', address_id, 0, 0, 5, '${imgUrl}'
+      SELECT
+        '${firstName}',
+        '${lastName}',
+        '${password}',
+        '${email}',
+        address_id,
+        0,
+        0,
+        null,
+        '${imgUrl}'
       FROM X;
     `;
 
@@ -44,6 +65,76 @@ const controllers = {
       }
     });
   },
+
+  addAnnouncement: (req, res) => {
+    const { userId } = req.params;
+    const {
+      announcementBody,
+      date,
+      time,
+    } = req.params;
+
+    const queryStr = `
+      INSERT INTO nexdoor.announcements (
+        user_id,
+        announcement_body,
+        date,
+        time
+      )
+      VALUES (
+        ${userId},
+        ${announcementBody},
+        ${date},
+        ${time}
+      )
+    `;
+
+    db.query(queryStr, (err, data) => {
+      if (err) {
+        res.status(400).send(err.stack);
+      } else {
+        res.status(200).send(data.rows);
+      }
+    });
+  },
+
+  getUser: (req, res) => {
+    const { id } = req.params;
+
+    const queryStr = `
+      SELECT
+        firstname,
+        lastname,
+        email,
+        karma,
+        task_count,
+        avg_rating,
+        profile_picture_url, (
+          SELECT JSON_AGG(ROW_TO_JSON(t))
+          FROM (
+            SELECT
+              street_address,
+              city,
+              state,
+              zipcode,
+              neighborhood
+            FROM nexdoor.address
+            WHERE address_id=nexdoor.users.address_id
+          ) t
+        ) as address
+      FROM nexdoor.users
+      WHERE user_id=${id};
+    `;
+
+    db.query(queryStr, (err, data) => {
+      if (err) {
+        res.status(400).send(err.stack);
+      } else {
+        res.status(200).send(data.rows);
+      }
+    });
+  },
+
 };
 
 module.exports = controllers;
--- server/router.js
+++ server/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const controllers = require('./controllers');
 
 router
+  .get('/user/:id', controllers.getUser)
   .post('/user', controllers.addUser);
 
 module.exports = router;
