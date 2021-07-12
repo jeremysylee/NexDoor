fe041926aa8e697bf1e6e32dd14801484e7a924e
Jimmy <jgarcia3000@gmail.com>
2021-07-12 10:17:24 -0700

    Created React Router component with routes

---
+++ client/src/components/MainFeed.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+const MainFeed = () => {
+
+  return (
+    <h1>Lemon</h1>
+  )
+}
+
+export default MainFeed;
---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+const SignUp = () => {
+
+  return (
+    <h1>hey</h1>
+  )
+}
+
+export default SignUp;
