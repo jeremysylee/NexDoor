f3f056fca5397e31d6dc8a86b1267f9dc5a25663
raphspies <raphspies@gmail.com>
2021-07-12 16:16:21 -0700

    added basic routing/validation for login

--- server/router.js
+++ server/router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 // import individual routes
-// const login = require('./routes/login');
+const login = require('./routes/login');
 const newuser = require('./routes/newuser');
 
 // route each endpoint
-// router.use('/login', login);
+router.use('/login', login);
 router.use('/newuser', newuser);
 
 module.exports = router;
