abec4887c66f2f7ad079be1e90f110ec530e6241
Jeremy Lee <jeremysylee@gmail.com>
2021-07-12 10:24:51 -1000

    Test Data created for development

--- client/src/components/MainFeed.jsx
+++ client/src/components/MainFeed.jsx
@@ -4,19 +4,16 @@ import { useSelector } from 'react-redux';
 import axios from 'axios';
 
 const MainFeed = () => {
+  // const [ratingModal, setRatingModal] = useState(false);
+  // const [requestModal, setRequestModal] = useState(false);
 
-  const [ratingModal, setRatingModal] = useState(false);
-  const [requestModal, setRequestModal] = useState(false);
+  const placeholder = 'placeholder';
 
   return (
-
-
     <div>
-      <h1>Lemon</h1>
-    <button>Rating</button>
-    <button></button>
+      <h1>{placeholder}</h1>
     </div>
-  )
-}
+  );
+};
 
 export default MainFeed;
