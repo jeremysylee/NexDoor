eaacd6467ff4639ed28f51a9c111c31383be0878
Miguel Buenviaje <migbuen44@gmail.com>
2021-07-12 22:17:38 -0700

    Implemented functional map component

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -6,11 +6,11 @@ const Map = () => {
   const [addresses, setAddresses] = useState(['6616 Cleon Ave North Hollywood CA']);
   const [coordinates, setCoordinates] = useState([]);
 
-  let coordinateContainer = [];
+  const coordinateContainer = [];
 
   const mapStyles = {
     height: '50vh',
-    width: '%100',
+    width: '75vh',
   };
 
   const defaultCenter = {
@@ -23,10 +23,8 @@ const Map = () => {
     const urlFormattedAddress = addressArr.join('+');
     const result = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${urlFormattedAddress}
     &key=AIzaSyAF8YxtZo1Y_VwXnNrmb1ErGpupP1kYniI`)
-      .then((res) => {
-        return res.data.results[0].geometry.location;
-      });
-      return result;
+      .then((res) => res.data.results[0].geometry.location);
+    return result;
   };
 
   let iterateAddressesAsync = async () => {
@@ -38,30 +36,19 @@ const Map = () => {
   };
 
   useEffect(() => {
-    // addresses.forEach(address => {
-    //   getCoordinates(address)
-    //     .then(res => {
-    //       console.log(res);
-    //     });
-    // });
     iterateAddressesAsync()
       .then(() => {
-        // console.log(coordinateContainer);
         setCoordinates(coordinateContainer);
       });
   }, []);
 
-  //get addresses
-  //put those addresses into addresses array
-  //iterate through addresses array
-    //convert addresses to lat lon object literal
-    //return marker with object literal in position value
   return (
     <LoadScript
-      googleMapsApiKey='AIzaSyAF8YxtZo1Y_VwXnNrmb1ErGpupP1kYniI'>
+      googleMapsApiKey="AIzaSyAF8YxtZo1Y_VwXnNrmb1ErGpupP1kYniI"
+    >
       <GoogleMap
         mapContainerStyle={mapStyles}
-        zoom={10.5}
+        zoom={10}
         center={defaultCenter}
       >
         {
