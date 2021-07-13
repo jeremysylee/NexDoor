e0243979cc8dc76fb106094166cbaf3e1c6aeac7
Miguel Buenviaje <migbuen44@gmail.com>
2021-07-12 22:08:57 -0700

    Completed functional map component

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React, {useState, useEffect} from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import axios from 'axios';
 
 const Map = () => {
+  const [addresses, setAddresses] = useState(['6616 Cleon Ave North Hollywood CA']);
+  const [coordinates, setCoordinates] = useState([]);
+
+  let coordinateContainer = [];
+
   const mapStyles = {
     height: '50vh',
     width: '%100',
@@ -12,16 +18,68 @@ const Map = () => {
     lng: -118.2437,
   };
 
+  const getCoordinates = async (address) => {
+    const addressArr = address.split(' ');
+    const urlFormattedAddress = addressArr.join('+');
+    const result = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${urlFormattedAddress}
+    &key=AIzaSyAF8YxtZo1Y_VwXnNrmb1ErGpupP1kYniI`)
+      .then((res) => {
+        return res.data.results[0].geometry.location;
+      });
+      return result;
+  };
+
+  let iterateAddressesAsync = async () => {
+    for (let address of addresses) {
+      let coordinate = await getCoordinates(address);
+      coordinateContainer.push(coordinate);
+      // console.log('coordinate: ', coordinate);
+    }
+  };
+
+  useEffect(() => {
+    // addresses.forEach(address => {
+    //   getCoordinates(address)
+    //     .then(res => {
+    //       console.log(res);
+    //     });
+    // });
+    iterateAddressesAsync()
+      .then(() => {
+        // console.log(coordinateContainer);
+        setCoordinates(coordinateContainer);
+      });
+  }, []);
+
+  //get addresses
+  //put those addresses into addresses array
+  //iterate through addresses array
+    //convert addresses to lat lon object literal
+    //return marker with object literal in position value
   return (
     <LoadScript
-    googleMapsApiKey='AIzaSyAF8YxtZo1Y_VwXnNrmb1ErGpupP1kYniI'>
-     <GoogleMap
-       mapContainerStyle={mapStyles}
-       zoom={10.5}
-       center={defaultCenter}
-     />
-  </LoadScript>
-    // <div>hi</div>
+      googleMapsApiKey='AIzaSyAF8YxtZo1Y_VwXnNrmb1ErGpupP1kYniI'>
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={10.5}
+        center={defaultCenter}
+      >
+        {
+          coordinates.map((coordinate, idx) => {
+            // console.log('coordinate: ', coordinate);
+            const position = {};
+            position.lat = coordinate.lat;
+            position.lng = coordinate.lng;
+            if (!coordinate) {
+              return;
+            }
+            return (
+              <Marker key={idx} position={position} />
+            );
+          })
+        }
+      </GoogleMap>
+    </LoadScript>
   );
 };
 
