3a203b7c7b07f73142b8c7c0041414f20766ce80
Jeremy Lee <jeremysylee@gmail.com>
2021-07-14 06:57:14 -1000

    conflicts resolved

--- client/src/components/MainFeed/index.jsx
+++ client/src/components/MainFeed/index.jsx
@@ -21,6 +21,7 @@ const MapPlaceholder = styled.div`
   align-items: center;
 `;
 
+<<<<<<< HEAD
 const MainFeed = () => (
   <Grid
     container
@@ -35,5 +36,42 @@ const MainFeed = () => (
     {/* <MapPlaceholder /> */}
   </Grid>
 );
+=======
+const MainFeed = () => {
+  const tasks = useSelector((store) => store.tasksReducer.tasks);
+  // const dispatch = useDispatch();
+  const placeholder = 'placeholder';
+
+  // const getTasks = () => {
+  //   axios.get('/api/tasks')
+  //     .then(({ data }) => {
+  //       console.log(data);
+  //       dispatch({
+  //         type: 'SET_TASKS',
+  //         tasks: data,
+  //       })
+  //         .catch((err) => console.error(err));
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   getTasks();
+  // }, []);
+
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="flex-start"
+      style={{ fontStyle: 'Roboto' }}
+    >
+      <Sidebar />
+      <Tasks />
+      <MapPlaceholder />
+    </Grid>
+  );
+};
+>>>>>>> cda59c84e648212048b390348f13e10d4d6d7789
 
 export default MainFeed;
