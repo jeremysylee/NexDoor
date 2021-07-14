26310e244e11c6d7a5e6c20e740afbbcd08593ea
raphspies <raphspies@gmail.com>
2021-07-14 13:48:25 -0700

    removed unused express-session text

--- server/index.js
+++ server/index.js
@@ -4,26 +4,15 @@ const cors = require('cors');
 const path = require('path');
 const router = require('./router');
 require('dotenv').config();
-const sessions = require('express-session');
 
 const app = express();
 const port = 3500;
-// const session = require('./routes/session');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 app.use(cors());
-
-app.use(sessions({
-  secret: process.env.SESSION_SECRET,
-  saveUninitialized: true,
-  resave: false,
-  // secure: true, // only enable if https is enabled
-  // store: new (require('connect-pg-simple')(sessions)),
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
-}));
 
 app.use('/api', router);
 
