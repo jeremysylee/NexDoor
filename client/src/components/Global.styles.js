20d4a802121f240c3c9a8ebdc032f7ad8245d096
Jeremy Lee <jeremysylee@gmail.com>
2021-09-30 16:10:55 -1000

    Added styling for messages. Added global style

---
+++ client/src/components/Global.styles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f1f2f5;
+  }
+`;
+
+export default GlobalStyle;
--- client/src/containers/Request/Chat/Message.jsx
+++ client/src/containers/Request/Chat/Message.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from '@material-ui/core';
+import { Grid, Avatar } from '@material-ui/core';
 import styled from 'styled-components';
 import { DateTime } from 'luxon';
 
@@ -10,89 +10,74 @@ const Row = styled.div`
   align-items: center;
 `;
 
+const Col = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: right;
+`;
+
+const MyText = styled.div`
+  text-align: right;
+  background-color: #4496B4;
+  border-radius: 22px;
+  width: fit-content;
+  padding: 6px 2px 6px 24px;
+  color: white;
+  font-size: 14px;
+  margin: 0.4em 3.5em;
+`;
+
+const YourText = styled(MyText)`
+  text-align: left;
+  background-color: white;
+  color: black;
+  margin-left: 10%;
+  margin: 10px 10px;
+  padding: 4px 31px 5px 19px;
+`;
+
+const MyTimeStamp = styled.div`
+  font-size: 9px;
+  color: white;
+`;
+const YourTimeStamp = styled(MyTimeStamp)`
+  color: grey;
+`;
+
 const Message = ({ message, user, isUser }) => {
-  let style;
-  const profilePic = 'm';
-  const dateStyle = {
-    fontSize: '10px',
-    color: 'grey',
-  };
-
-  const dateStyle2 = {
-    fontSize: '10px',
-    color: 'white',
-    marginTop: '-5px',
-  };
-
-  const profilePicStyle = {
-    height: '25px',
-    width: '25px',
-    backgroundColor: '#bbb',
-    borderRadius: '50%',
-    display: 'inline-block',
-    textAlign: 'center',
-  };
-
-  const messageStyle = {
-    margin: '10px',
-  };
-
   if (isUser) {
-    style = {
-      textAlign: 'right',
-      margin: '10px 10px',
-      height: '8vh',
-      backgroundColor: '#F2F1F7',
-      borderRadius: '22px',
-      width: 'fit-content',
-      padding: '11px 14px 8px',
-    };
-
     return (
       <Grid container display="flex" justifyContent="flex-end">
-        <div style={style}>
-          <span style={messageStyle}>{message.message_body}</span>
-          <div style={profilePicStyle}>{user.firstname.slice(0, 1)}</div>
-          <div style={dateStyle}>
-            {`${DateTime.fromISO(message.date).toFormat('ccc')} ${DateTime.fromISO(message.time).toFormat('t')}`}
-          </div>
-          {/* <span>{message.time}</span> */}
-        </div>
+        <MyText>
+          <Row>
+            <Col style={{ marginRight: '15px' }}>
+              <span>{message.message_body}</span>
+              <MyTimeStamp>
+                {`${DateTime.fromISO(message.date).toFormat('ccc')} ${DateTime.fromISO(message.time).toFormat('t')}`}
+              </MyTimeStamp>
+            </Col>
+          </Row>
+        </MyText>
       </Grid>
     );
   }
 
-  style = {
-    marginLeft: '10%',
-    margin: '10px 10px',
-    backgroundColor: '#414292',
-    borderRadius: '22px',
-    padding: '2px 24px 9px',
-    color: 'white',
-  };
   return (
     <Grid container justifyContent="flex-start">
-      <div style={style}>
-        <Row>
-          <span style={profilePicStyle}>{profilePic}</span>
-          <div style={messageStyle}>{message.message_body}</div>
-        </Row>
-        <div style={dateStyle2}>
-          {`${DateTime.fromISO(message.date).toFormat('ccc')} ${DateTime.fromISO(message.time).toFormat('t')}`}
-        </div>
-        {/* <span>{message.time}</span> */}
-      </div>
+      <Row>
+        <Avatar src={user.profile_picture_url} alt={user.firstname.slice(0, 1)} />
+        <YourText>
+          <Col>
+            <div>{message.message_body}</div>
+            <YourTimeStamp>
+              {`${DateTime.fromISO(message.date).toFormat('ccc')} ${DateTime.fromISO(message.time).toFormat('t')}`}
+            </YourTimeStamp>
+          </Col>
+          {/* <span>{message.time}</span> */}
+        </YourText>
+      </Row>
     </Grid>
   );
-
-  // console.log(message.message_body)
-  // return (
-  //   <div style={style}>
-
-  //     <div>{message.message_body}</div>
-  //     {/* <span>{message.time}</span> */}
-  //   </div>
-  // );
 };
 
 Message.propTypes = {
@@ -109,6 +94,7 @@ Message.propTypes = {
     firstname: PropTypes.string,
     lastname: PropTypes.string,
     user_id: PropTypes.number,
+    profile_picture_url: PropTypes.string,
   }).isRequired,
   isUser: PropTypes.bool.isRequired,
 };
