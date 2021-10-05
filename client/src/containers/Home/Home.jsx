fe172fb4ccc7a36c9d542168290bd1fec6f4d6c9
Jeremy Lee <jeremysylee@gmail.com>
2021-10-04 16:07:01 -1000

    stickied map

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-import styled from 'styled-components';
 
 import Header from '../../components/Header';
 import RequestFeed from '../../features/Request/RequestFeed';
 import Sidebar from '../../components/Sidebar';
 import Map from '../../features/Map/Map';
 import { Body } from '../../components/App.styles';
-
-const MapContainer = styled.div`
-  position: sticky;
-  top: 20;
-  min-width: 300px;
-  max-width: 650px;
-  height: 95vh;
-  border-radius: 20px;
-  // box-shadow: 0 8px 16px 0 #BDC9D7;
-  margin-top: 2em;
-  flex-grow: 4;
-  flex-shrink: 2;
-`;
 
 const Home = () => (
   <Body>
@@ -37,9 +23,7 @@ const Home = () => (
       >
         <Sidebar />
         <RequestFeed />
-        <MapContainer>
-          <Map />
-        </MapContainer>
+        <Map />
       </Grid>
     </Grid>
   </Body>
