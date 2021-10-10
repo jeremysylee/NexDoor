6fb0cfa831627c6a59f1b3c97aefa9c112c0372c
Jeremy Lee <jeremysylee@gmail.com>
2021-10-10 08:04:44 -1000

    Restructured file structure.

--- client/src/features/Request/RequestTile/index.js
+++
@@ -1,7 +0,0 @@
-import MyRequestTile from './MyRequestTile/MyRequestTile';
-import MyTaskTile from './MyTaskTile/MyTaskTile';
-import OpenTaskTile from './OpenTaskTile/OpenTaskTile';
-
-export {
-  MyRequestTile, MyTaskTile, OpenTaskTile,
-};
